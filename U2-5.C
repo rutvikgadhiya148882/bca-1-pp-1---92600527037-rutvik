efc916cb70d2f1c3af5a5dcd4ee50f33dbef7947
rutvikgadhiya148882 <rutvik.gadhiya148882@marwadiuniversity.ac.in>
2026-07-31 10:58:57 +0530

    Add files via upload

---
+++ U2-5.C
@@ -0,0 +1,23 @@
+//wap which calcutes area of rectangle
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int l,b,a;
+	clrscr();
+	printf("\n enter value of l : ");
+	scanf("%d",&l);
+
+	printf("\n enter value of b : ");
+	scanf("%d",&b);
+
+	a = l*b;
+
+	printf("\n area of rectangle is: %d" ,a);
+
+	getch();
+
+}
+
---
+++ U2-6.C
@@ -0,0 +1,19 @@
+//wap which cal avg
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+	int x,y,z,avg,total;
+	clrscr();
+	printf("\n enter value of x,y,z:");
+	scanf("%d%d%d",&x,&y,&z);
+
+	//total =x+y+z;
+	avg =(x+y+z)/3;
+	printf("\n average is :%d",avg);
+	getch();
+
+}

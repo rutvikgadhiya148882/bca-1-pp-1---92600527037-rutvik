48d648a0f4c9dd1b6718424ee9f879c0ad9e289b
rutvikgadhiya148882 <rutvik.gadhiya148882@marwadiuniversity.ac.in>
2026-08-06 15:45:39 +0530

    Add files via upload

---
+++ U3-1.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if (a > b)
+        printf("Maximum = %d", a);
+    else
+        printf("Maximum = %d", b);
+
+    return 0;
+}

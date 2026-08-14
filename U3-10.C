427a6b3b11a0882e0254cb27b4f4e0f6d5ea31bd
rutvikgadhiya148882 <rutvik.gadhiya148882@marwadiuniversity.ac.in>
2026-08-14 10:56:30 +0530

    Add files via upload

---
+++ U3-10.C
@@ -0,0 +1,16 @@
+//display ascii code of input character
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	char x;
+	clrscr();
+	printf("\n enter any character : ");
+	scanf("%c" ,&x); //A = 65
+
+	printf("\n input character is : %c" ,x+32);
+	printf("\n ascii code is : %d" ,x+32);
+	getch();
+
+}
